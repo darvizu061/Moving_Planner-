c6cfb75db2c9c8d323b625010282d2a09c2cbfd7
darvizu061 <danny081696arvizu@gmail.com>
2015-12-30 19:06:30 +0000

    Updated Readme.md, updated script.js to use Wikipedia api and load content to DOM.

--- README.md
+++ README.md
@@ -6,7 +6,15 @@ Moving planner is an online web app used to showcase general information about a
 
 This project was initially created by udacity.com as part of their INTRO TO AJAX course online. The init files were created by udacity.com. 
 
-The key file in use in this porject is script.js which runs the multiple api's needed in order for the app to work. I take full credit for the script.js file.
+The key file in use in this porject is script.js which runs the multiple api's needed in order for the app to work. I take full credit in creating the script.js file.
 
+**The New York Times API**
 
+Unfortunately, since this web app requires the New York Times Article Search API to load various articles, you will need to register for an API key at http://developer.nytimes.com/docs/read/reference/keys .
+
+After you obtain a key, go into js/script.js. Go to line 23, copy and paste your key into the `var nyApiKey`, save file and you should be good to go. 
 ##### Copyright and Public Usage 
+
+Ownership of the init files(which is everything exept the js/script.js file) belongs to udacity.com. 
+
+My file, js/script.js, is free of any copyright laws. Feel free to use!
--- js/script.js
+++ js/script.js
@@ -20,11 +20,50 @@ function loadData() {
     $greeting.text("So, you want to live in " + address + '?');
     $body.append('<img class="bgimg" src="'+streetViewUrl+'">');
     //load New York Times' Articles 
-    var NYTapiurl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q='+cityValue+'&sort=newest&api-key=3efc62d1c3f59f3f56ba7f84549ab6c1:4:73883147';
-    
+    var nyApiKey = "3efc62d1c3f59f3f56ba7f84549ab6c1:4:73883147";
+    var NYTapiurl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q='+cityValue+'&sort=newest&api-key='+nyApiKey;
     $.getJSON(NYTapiurl, function (data){
-        console.log(data);
+        var articles = data.response.docs;
+        var article;
+        $nytHeaderElem.text('New York Times Articles About '+cityValue);
+        
+        for(var i = 0;i<articles.length; i++){
+            article = articles[i];
+            $nytElem.append('<li class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a>'+'<p>'+article.snippet+'</p>'+'</li>');
+        }
+        
+        
+    }).error(function(e){
+        $nytHeaderElem.text('New York Times Articles Could Not Generate');
+        
     });
+    //create wiki request timeout 
+    var wikiRequestTimeout = setTimeout(function() {$wikiElem.text("Failed to Generate Wikipedia Articles");}, 6000);
+    //load Wikipedia url
+    var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='+cityValue+'&format=json&callback=wikiCallback'; 
+    $.ajax( {
+        url: wikiUrl,
+        dataType: 'jsonp',
+        success: function(response) {
+            // do something with response
+            var wikiResults = response[1];
+            //extractions for faster for-loop below
+            var i;
+            var wikiResultsLength = wikiResults.length;
+            var articleResult; 
+            var articleUrl;
+            
+            for(i=0; i<wikiResultsLength; i++){
+                articleResult = wikiResults[i];
+                articleUrl = 'http://en.wikipedia.org/wiki/'+articleResult;
+                //append to page 
+                $wikiElem.append('<li><a href="'+articleUrl+'">'+articleResult+'</a></li>');
+            }
+            clearTimeout(wikiRequestTimeout);
+        }
+    } );
+    
+    
 
     return false;
 };
